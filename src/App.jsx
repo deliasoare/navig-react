1cfb5ac14df40668bb7b4824e68d9d3aeb26fe84
deliasoare <deliasoare006@yahoo.com>
2024-07-23 14:02:35 +0300

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
+import Header from './components/Header';
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const [isLogged, setIsLogged] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeDropdown = () => {
+    if (isOpen)
+      setIsOpen(false);
+  } 
+
+    return (
+    <div className='container' onClick={closeDropdown}>
+      <Header isLogged={isLogged} isOpen={isOpen} setIsOpen={setIsOpen}> </Header>
+      <div className="main">lala</div>
+      <div className="footer">lala</div>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import '../styles/header.scss';
+
+import { useState } from 'react';
+import Photo from '../assets/LOGO_NAVIGAID.png'
+
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { BsFillFilePersonFill } from "react-icons/bs";
+
+function Header({isLogged, isOpen, setIsOpen}) {
+
+    const toggleMenu = (e) => {
+        setIsOpen(!isOpen);
+      };
+
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src={Photo} alt="" />
+                <div className="name">NavigAid</div>
+            </div>
+            <div className="categories">
+                <div className="discover">Discover</div>
+                <div className="review">Review</div>
+            </div>
+             <div className="user-menu">
+                <button className="user-icon" onClick={toggleMenu}>
+                    <div className="icons">
+                        <RxHamburgerMenu size={30}/>
+                        <BsFillFilePersonFill size={30}/>
+                    </div>
+                </button>
+                {isOpen && (
+                    <div className="dropdown-menu">
+                        {isLogged ?
+                            <div className="logged">
+                                <div className="menu-item">Profile</div>
+                                <div className="menu-item">Logout</div>
+
+                            </div>
+                        :
+                            <div className="n-logged">
+                                <div className="menu-item">Login</div>
+                                <div className="menu-item">Register</div>
+                            </div>
+                        }
+                        
+                    </div>
+                )}
+                </div>
+        </div>
+    );
+}
+
+export default Header;
